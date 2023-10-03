2600b5065d6facec81069066ffe4618fec705b4a
skee21 <shushantshadow2122@gmail.com>
2023-10-03 12:22:23 +0000

    fixed some bugs, and got some bugs. saving them for later

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from typing import Optional
 import discord
 from discord import app_commands
 import os
-
 from discord import interactions
 from keep_alive import keep_alive
+from discord import Member
 import replit
 
 intents = discord.Intents.default()
@@ -15,7 +15,6 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 db = replit.db
-db["players"] = []
 
 @client.event
 async def on_ready():
@@ -30,7 +29,7 @@ async def hello(interaction):
 @tree.command(name = "database", description= "devs only command")
 async def database(interaction):
   if interaction.user.id == 1077648874002460672:
-    users = db["players"]
+    users = dict(db)
     await interaction.response.send_message(users)
   else:
     await interaction.response.send_message("know your place hooman.")
@@ -38,22 +37,24 @@ async def database(interaction):
 @tree.command(name= "reset", description= "thanos snap lol")
 async def reset(interaction):
   if interaction.user.id == 1077648874002460672:
-    users = db["players"]
-    del users
+    del db[]
   else:
     await interaction.response.send_message("know your place hooman.")
 
 @tree.command(name= "register", description= "Neko will remember you <3")
 async def register(interaction):
   userid = str(interaction.user.id)
-  if userid in db["players"]:
+  registered_users = db.get() 
+  
+  if userid in registered_users:
     await interaction.response.send_message("Got short term memory loss?")
   else:
-    db["players"].append(userid)
-    await interaction.response.send_message("you are registered now, yayy!")
+    registered_users.append(userid)
+    db[] = registered_users 
+    await interaction.response.send_message("You are registered now, yayy!")
 
 @tree.command(name="avatar", description="shows avatar")
-async def avatar(interaction, user: Optional[discord.Member] = None):
+async def avatar(interaction, user: Optional[Member] = None):
   if user is None:
     user = interaction.user
     embed = discord.Embed(title=f"pfp of {user.display_name}", color=0xe91e63)
@@ -65,24 +66,29 @@ async def avatar(interaction, user: Optional[discord.Member] = None):
     await interaction.response.send_message(embed=embed)
 
 @tree.command(name="profile")
-async def profile(interaction, user:discord.Member):
-  user_id = str(user.id)
-  members = db.get("members", {})
-  user_data = members.get(user_id, {})
-  xp = user_data.get("xp", 1) 
-  level = xp // 100
-  army_space = user_data.get("army_space", 30)
+async def profile(interaction, user: Optional[Member] = None):
+  if user is None:
+    user = interaction.user
   
+  user_id = str(user.id)
+  print(members)
+
+  user_data = db.get(user_id, None)
+  if user_data is None:
+    await interaction.response.send_message("User is not registered probably.")
+    return
+
+  xp = user_data.get("xp", 1) 
+  army_space = user_data.get("army_space", 30)
 
   embed = discord.Embed(title=f"Profile of {user.display_name}", color=0xe91e63)
   embed.set_thumbnail(url=user.display_avatar)
-  embed.add_field(name="Level:", value=level, inline=True)
+  embed.add_field(name="Level:", value=1, inline=True)
   embed.add_field(name="XP:", value=f"{xp}/100", inline=True)
   embed.add_field(name="Clan:", value="in dev", inline=False)
   embed.add_field(name="Army space:", value=army_space)
-  
-  await interaction.response.send_message(embed=embed)
 
+  await interaction.response.send_message(embed=embed)
 
 keep_alive()
 
